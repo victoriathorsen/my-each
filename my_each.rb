9b413dadf3ca4cc62048a21337810390d9c9a214
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-13 19:30:54 -0800

    commit tree 8a1ea9ec79847ac88aa920f5f6f698468e69c358

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words) # put argument(s) here
+  if block_given?
+    i=0
+  
+  while i < words.length
+    yield(words[i])
+    i += 1
+  end
+  words
+
+  end
 end
